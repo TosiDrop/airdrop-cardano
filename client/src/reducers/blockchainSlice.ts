1624b85c2900d1bba9e275a86762233872dab521
aryelciu001 <aryelciu001@gmail.com>
2022-03-10 13:50:12 +0800

    chore: cleanup

--- client/src/reducers/blockchainSlice.ts
+++ client/src/reducers/blockchainSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { BlockchainState, WalletName } from "utils";
+import { BlockchainState } from "utils";
 
 const initialState: BlockchainState = {
   api: undefined,
