20b09d7349a5050afcbcade850a1d02c1c1d085d
aryelciu001 <aryelciu001@gmail.com>
2022-03-15 23:50:43 +0800

    chore: send request to server to /validate and /submit airdrop

--- client/src/components/AirdropTool/index.tsx
+++ client/src/components/AirdropTool/index.tsx
@@ -8,8 +8,10 @@ import useDualThemeClass from "hooks/useDualThemeClass";
 import { AddressAmountMap, AirdropRequestBody, Token, PopUpType } from "utils";
 import { useState } from "react";
 import axios from "axios";
+import { TransactionUnspentOutput, Transaction, TransactionWitnessSet } from "@emurgo/cardano-serialization-lib-asmjs";
 import "./index.scss";
 
+const Buffer = require("buffer/").Buffer;
 const COMPONENT_CLASS = "airdrop-tool";
 
 export default function AirdropTool() {
@@ -32,7 +34,7 @@ export default function AirdropTool() {
     text: "",
   });
 
-  const sendToken = () => {
+  const sendToken = async () => {
     setPopUpProps({
       show: true,
       type: PopUpType.LOADING,
@@ -42,30 +44,32 @@ export default function AirdropTool() {
     const requestBody = prepareBody(walletAddress, selectedToken, addressArray);
     const url = process.env.REACT_APP_API_TX;
 
-    axios
-      .post(`${url}/api/v0/validate`, requestBody)
-      .then((res) => {
-        console.log(res);
-        setPopUpProps({
-          show: true,
-          type: PopUpType.LOADING,
-          text: `Sending ${totalAmountToAirdrop} ${selectedToken.name}`,
-        });
-        axios.post(`${url}/api/v0/submit`, requestBody).then((res) => {
-          console.log(res);
-        });
-      })
-      .catch((e) => {
-        switch (e.response.status) {
-          case 406:
-            setPopUpProps({
-              show: true,
-              type: PopUpType.FAIL,
-              text: "Balance in wallet is not enough",
-            });
-            return;
-        }
+    try {
+      await axios.post(`${url}/api/v0/validate`, requestBody)
+      setPopUpProps({
+        show: true,
+        type: PopUpType.LOADING,
+        text: `Sending ${totalAmountToAirdrop} ${selectedToken.name}`,
       });
+      const submitAirdrop = await axios.post(`${url}/api/v0/submit`, requestBody)
+      const cborHexInString = submitAirdrop.data
+      /**
+       * Hi TOM! :)
+       * please continue here
+       */      
+    } catch (e: any) {
+      console.log(e)
+      switch (e.response?.status) {
+        case 406: {
+          setPopUpProps({
+            show: true,
+            type: PopUpType.FAIL,
+            text: "Balance in wallet is not enough",
+          });
+          return;
+        }
+      }
+    }
   };
 
   return (
@@ -113,42 +117,3 @@ function prepareBody(
   };
   return body;
 }
-
-/*
-const submit = async (API: any, signed_transaction: any) => {
-  //convert hex string to bytes
-  const txCli = Transaction.from_bytes(Buffer.from(signed_transaction, "hex"));
-  //console.log("cbor bytes",txCli)
-  const txBody = txCli.body();
-  const witnessSet = txCli.witness_set();
-  //this clears the dummy signature from the transaction
-  witnessSet.vkeys()?.free();
-  const transactionWitnessSet = TransactionWitnessSet.new();
-  console.log(transactionWitnessSet);
-  //build new unsigned transaction
-  const tx = Transaction.new(
-    txBody,
-    TransactionWitnessSet.from_bytes(transactionWitnessSet.to_bytes())
-  );
-  //send to wallet for signature
-  let txVkeyWitnesses = await api.signTx(
-    Buffer.from(tx.to_bytes(), "utf8").toString("hex"),
-    true
-  );
-  txVkeyWitnesses = TransactionWitnessSet.from_bytes(
-    Buffer.from(txVkeyWitnesses, "hex")
-  );
-  transactionWitnessSet.set_vkeys(txVkeyWitnesses.vkeys());
-  const signedTx = Transaction.new(tx.body(), transactionWitnessSet);
-  // convert array to hex string
-  const hexSigned = Buffer.from(signedTx.to_bytes(), "utf8").toString("hex");
-  console.log(hexSigned);
-  //submit transaction
-  //const submittedTxHash =  await api.submitTx(Buffer.from(signedTx.to_bytes(), "utf8").toString("hex"));
-  //window.alert("submitted, hash = "+ submittedTxHash)
-
-  return {
-    submit: submit,
-  };
-};
-*/
